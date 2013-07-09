295a91aa8d2648a360fa7bd8131e83ff994ff3b5
Ubuntu <ubuntu@ip-172-31-30-203.us-west-2.compute.internal>
2013-07-09 00:59:27 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var content = fs.readFileSync(index.html, encoding);
+var content = fs.readFileSync(index.html, "utf-8");
 
 app.get('/', function(request, response) {
 // response.send('Hello World2!');
