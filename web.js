80a09e9a88748f889a2d9c800c89d778ca16dc06
Ubuntu <ubuntu@ip-172-31-30-203.us-west-2.compute.internal>
2013-07-10 14:36:42 +0000

    Updated web.js 3

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-var var fs = require('fs');
-var content1 = fs.readFileSync('~/bitstarter/index.html', 'utf-8');
-
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-//   response.send('content1');
+//  response.send('Hello World2!');
+
+    var var fs = require('fs');
+    var content1 = fs.readFileSync('~/bitstarter/index.html', 'utf-8');
+    response.send('content1');
 });
 
 var port = process.env.PORT || 5000;
